9583f081c0ce872d9b84564ba17c013cbb4a95c1
anaakro <117375234+anaakro@users.noreply.github.com>
2022-12-15 18:01:37 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ function addRow() {
 	var td1 = document.createElement("td");
 	var td2 = document.createElement("td");
 	var td3 = document.createElement("td");
-	int td4 = document.createElement("td");
+	var td4 = document.createElement("td");
 	var row = document.createElement("tr");
 		
 		
